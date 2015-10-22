ed76c5f39bd4e8441c0d8e89cb4f3ea59f41b2c8
maximili.jeannere <maximili.jeannere@IG13-JeannereM.intra.eiaj.ch>
2015-10-22 09:15:45 +0200

    Modif classe bank

--- src/ch/hearc/ig/odi/customeraccount/business/Bank.java
+++ src/ch/hearc/ig/odi/customeraccount/business/Bank.java
@@ -5,28 +5,28 @@ import ch.hearc.ig.odi.customeraccount.business.Customer;
 import ch.hearc.ig.odi.customeraccount.business.Account;
 
 public class Bank {
-	private int _number;
-	private String _name;
-	private HashSet<Customer> _customers = new HashSet<Customer>();
-	private HashSet<Account> _accounts = new HashSet<Account>();
+	private int number;
+	private String name;
+	private HashSet<Customer> customers = new HashSet<Customer>();
+	private HashSet<Account> accounts = new HashSet<Account>();
 
-	public Bank(int aNumber, String aName) {
+	public Bank(int number, String name) {
 		throw new UnsupportedOperationException();
 	}
 
-	public Account getAccountByNumber(String aNumber) {
+	public Account getAccountByNumber(String number) {
 		throw new UnsupportedOperationException();
 	}
 
-	public Customer getCustomerByNumber(int aNumber) {
+	public Customer getCustomerByNumber(int number) {
 		throw new UnsupportedOperationException();
 	}
 
-	public void addAccount(int aNumber, String aFirstName, String aLastName) {
+	public void addAccount(int number, String firstName, String lastName) {
 		throw new UnsupportedOperationException();
 	}
 
-	public void addCustomer(String aNumber, String aName, double aRate, Customer aCustomer) {
+	public void addCustomer(String number, String name, double rate, Customer customer) {
 		throw new UnsupportedOperationException();
 	}
 }
